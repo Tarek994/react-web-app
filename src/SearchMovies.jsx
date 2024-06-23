956067bc8650e1d6720c9f517a0bfa464a9701b1
Tarek994 <ghali.tarek.123@hotmail.com>
2024-06-23 16:14:47 +0300

    feat(searchmovie-api): add the api and fetch data

---
+++ src/SearchMovies.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function SearchMovies() {
+  const [movieTitle, setMovieTitle] = useState("");
+
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    const url = `http://www.omdbapi.com/?i=tt3896198&apikey=adc8afc5=${movieTitle}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={searchMovie}>
+        <label htmlFor="query"> Movie Title</label>
+        <input
+          type="text"
+          name="query"
+          value={movieTitle}
+          onChange={(e) => setMovieTitle(e.target.value)}
+          placeholder="Enter Movie Title"
+          role="search"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+export default SearchMovies;
