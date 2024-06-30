5a51c5b086923bd017fbd23b905c61cc5d42828e
Tarek994 <ghali.tarek.123@hotmail.com>
2024-06-30 06:41:18 +0300

    feat(App.js): added state management for favourites and integrated with SearchMovies and FavouritesList components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import SearchMovies from './SearchMovies';
+import { useState } from "react";
+import "./App.css";
+import SearchMovies from "./SearchMovies";
+import FavouritesList from "./FavouritesList";
 
 function App() {
+  const [favourites, setFavourites] = useState([]);
+
+  const addFavourites = (movie) => {
+    setFavourites([...favourites, movie]);
+  };
+
+  console.log(favourites);
   return (
     <div className="App">
-      <SearchMovies/>
+      <SearchMovies addFavourites={addFavourites} />
+      <FavouritesList favourites={favourites} />
     </div>
   );
 }
